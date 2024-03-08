d45fd10d9e6f844598129b1982a34b2a709469e5
Salman Abid <theory252@gmail.com>
2024-03-09 00:30:36 +0500

    Display Character Dialogue using Raycasting
    This includes when our Player interact with a NPC (Non player character). NPC than show some instructions which are only available for few seconds to guide the player what to do.

--- Adventure Game/Assets/Script/PlayerController.cs
+++ Adventure Game/Assets/Script/PlayerController.cs
@@ -5,25 +5,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  // Variables related to player character movement
-  public InputAction MoveAction;
-  Rigidbody2D rigidbody2d;
-  Vector2 move;
-  public float speed = 3.0f;
+   // Variables related to player character movement
+   public InputAction MoveAction;
+   Rigidbody2D rigidbody2d;
+   Vector2 move;
+   public float speed = 3.0f;
 
-  // Variables related to the health system
-  public int maxHealth = 5;
-  int currentHealth;
-  public int health { get { return currentHealth; }}
+   // Variables related to the health system
+   public int maxHealth = 5;
+   int currentHealth;
+   public int health { get { return currentHealth; }}
 
-  // Variables related to temporary invincibility
-  public float timeInvincible = 2.0f;
-  bool isInvincible;
-  float damageCooldown;
+   // Variables related to temporary invincibility
+   public float timeInvincible = 2.0f;
+   bool isInvincible;
+   float damageCooldown;
 
-  // Variables related to animation
-  Animator animator;
-  Vector2 moveDirection = new Vector2(1,0);
+   // Variables related to animation
+   Animator animator;
+   Vector2 moveDirection = new Vector2(1,0);
+
+   // NPC commands
+   public InputAction talkAction;
+
 
    // Variables related to projectile
    public GameObject projectilePrefab;
@@ -31,11 +35,14 @@ public class PlayerController : MonoBehaviour
    public float ProjectileSpeed = 300;
 
   // Start is called before the first frame update
-  void Start()
-  {
+  void Start() {
      MoveAction.Enable();
      launchAction.Enable();
+     talkAction.Enable();
+
      launchAction.performed += Launch;
+     talkAction.performed += FindFriend;
+
      rigidbody2d = GetComponent<Rigidbody2D>();
      animator = GetComponent<Animator>();
 
@@ -43,57 +50,63 @@ public class PlayerController : MonoBehaviour
   }
  
   // Update is called once per frame
-  void Update()
-  {
-     move = MoveAction.ReadValue<Vector2>();
+  void Update() {
+      move = MoveAction.ReadValue<Vector2>();
 
-      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
-        {
-           moveDirection.Set(move.x, move.y);
-           moveDirection.Normalize();
-        }
+      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f)) {
+         moveDirection.Set(move.x, move.y);
+         moveDirection.Normalize();
+      }
 
-     animator.SetFloat("Look X", moveDirection.x);
-     animator.SetFloat("Look Y", moveDirection.y);
-     animator.SetFloat("Speed", move.magnitude);
+      animator.SetFloat("Look X", moveDirection.x);
+      animator.SetFloat("Look Y", moveDirection.y);
+      animator.SetFloat("Speed", move.magnitude);
 
-     if (isInvincible)
-       {
-           damageCooldown -= Time.deltaTime;
-           if (damageCooldown < 0)
-               isInvincible = false;
-       }
+      if (isInvincible) {
+         damageCooldown -= Time.deltaTime;
+         if (damageCooldown < 0)
+         isInvincible = false;
+      }
    }
 
-// FixedUpdate has the same call rate as the physics system
-  void FixedUpdate()
-  {
-     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
-     rigidbody2d.MovePosition(position);
-  }
+   // FixedUpdate has the same call rate as the physics system
+   void FixedUpdate() {
+      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+      rigidbody2d.MovePosition(position);
+   }
 
-  public void ChangeHealth (int amount)
-  {
-     if (amount < 0)
-       {
-           if (isInvincible)
-               return;
+   public void ChangeHealth (int amount) {
+      if (amount < 0)
+         {
+            if (isInvincible)
+                  return;
           
-           isInvincible = true;
-           damageCooldown = timeInvincible;
-           animator.SetTrigger("Hit");
-       }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
+         }
 
-     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-     UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
-  }
+      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+      UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+   }
 
-  void Launch(InputAction.CallbackContext context) {
-   GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+   void Launch(InputAction.CallbackContext context) {
+      GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
-   Projectile projectile = projectileObject.GetComponent<Projectile>();
-   projectile.Launch(moveDirection, ProjectileSpeed);
-   animator.SetTrigger("Launch");
-  }
+      Projectile projectile = projectileObject.GetComponent<Projectile>();
+      projectile.Launch(moveDirection, ProjectileSpeed);
+      animator.SetTrigger("Launch");
+   }
 
+   void FindFriend(InputAction.CallbackContext context) {
+      RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, moveDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+      if (hit.collider != null) {
+         NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+
+         if (character != null) {
+            UIHandler.instance.DisplayDialogue();
+         }
+      }
+   }
 }
--- Adventure Game/Assets/Script/UIHandler.cs
+++ Adventure Game/Assets/Script/UIHandler.cs
@@ -9,6 +9,11 @@ public class UIHandler : MonoBehaviour
     private VisualElement m_Healthbar;
     public static UIHandler instance {get; private set;}
 
+    // NPC overlays!
+    public float displayTime = 4.0f;
+    private VisualElement m_NonPlayerDialogue;
+    private float m_TimerDisplay;
+
     // Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
     private void Awake()
     {
@@ -20,10 +25,29 @@ public class UIHandler : MonoBehaviour
         UIDocument uiDocument = GetComponent<UIDocument>();
         m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
         SetHealthValue(1.0f);
+
+        // NPC overlays
+        m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
+        m_NonPlayerDialogue.style.display = DisplayStyle.None;
+        m_TimerDisplay = -1.0f;
+    }
+
+    void Update() {
+        if (m_TimerDisplay > 0) {
+            m_TimerDisplay -= Time.deltaTime;
+
+            if (m_TimerDisplay < 0) {
+                m_NonPlayerDialogue.style.display = DisplayStyle.None;
+            }
+        }
+    }
+
+    public void DisplayDialogue() {
+        m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
+        m_TimerDisplay = displayTime;
     }
     
-    public void SetHealthValue(float percentage)
-    {
+    public void SetHealthValue(float percentage) {
         m_Healthbar.style.width = Length.Percent(100 * percentage);
     }
 }
